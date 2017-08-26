2f375afcf487efb523356495504bca76811abff2
Alexandra Corremans <alexandra.corremans@gmail.com>
2017-08-26 22:00:36 +0200

    solution to 3

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+def echo text
+  return text
+end
+
+def shout text
+  return text.upcase
+end
+
+def repeat(word, number=2)
+  text = (word + ' ')*number
+  return text.chomp(' ')
+end
+
+def start_of_word(word, number)
+  return word[0..(number - 1)]
+end
+
+def first_word text
+  space = text.index(' ') - 1
+  return text[0..space]
+end
+
+def titleize text
+  list = text.split(' ')
+  list[0] = list[0].capitalize
+  list.map! do |word|
+    if word == "and" or word == "over" or word == "the"
+      word = word
+    else
+      word = word.capitalize
+    end
+  end
+  return list.join(' ')
+end
