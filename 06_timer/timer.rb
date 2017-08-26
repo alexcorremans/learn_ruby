8ba423d154a1344413099d65bf38f01d0e51972f
Alexandra Corremans <alexandra.corremans@gmail.com>
2017-08-27 00:30:36 +0200

    solution to 6

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds/3600
+    rest = @seconds%3600
+    minutes = rest/60
+    seconds = rest%60
+    return sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+  end
 end
