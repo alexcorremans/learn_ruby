d45a521bdd0d652c2286b73ac21fea408c4ff935
Alexandra Corremans <alexandra.corremans@gmail.com>
2017-08-26 22:34:27 +0200

    solution to 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
-#write your code here
+def translate text
+    list = text.split(' ')
+    list.map! do |word|
+      if ["a", "e", "i", "o", "u"].include? word[0]
+        word = word + "ay"
+      else
+        if word[0..1] == "qu"
+          word = word[2..-1] + "quay"
+        elsif word[1..2] == "qu"
+          word = word[3..-1] + word[0] + "quay"
+        elsif ["a", "e", "i", "o", "u"].include? word[1]
+          word = word[1..-1] + word[0] + "ay"
+        elsif not ["a", "e", "i", "o", "u"].include? word[2]
+          word = word[3..-1] + word[0..2] + "ay"
+        elsif not ["a", "e", "i", "o", "u"].include? word[1]
+          word = word[2..-1] + word[0..1] + "ay"
+        end
+      end
+    end
+    return list.join(' ')
+end
