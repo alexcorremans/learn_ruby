765e33901b1a3504d2e07bfad83c0b3c4dafaeea
Alexandra Corremans <alexandra.corremans@gmail.com>
2017-08-26 23:01:17 +0200

    solution to 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @list = @title.split(' ')
+    @list[0] = @list[0].capitalize
+    @list.map! do |word|
+      if ["the","a","an","and","in","of"].include? word
+        word = word
+      else
+        word = word.capitalize
+      end
+    end
+    return @list.join(' ')
+  end
 end
